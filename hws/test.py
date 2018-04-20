9327dd2110a4a0a9d8737dad2f01cd12392659f7
Pooran Singh Negi <pooran.negi@gmail.com>
2018-04-20 16:19:22 -0600

    added hw 6

---
+++ hws/test.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+
+################### Starting code written by Moran ###################
+
+# write code here
+
+
+
+
+
+#################### Starting code written by Jingru #################
+
+# write code here
+    
